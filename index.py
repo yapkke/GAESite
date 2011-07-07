8cac2b0761f9159f4570ef0322cd1e1b20d07138
KK <ykk@kk-alien>
2011-07-06 18:22:20 -0700

    Reading from DB and so on

--- index.py
+++ index.py
@@ -2,13 +2,68 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
-
+import gdata.spreadsheet.text_db as ss
+import yapkke.features as features
 import os
+
+spreadsheet="yapkke.appspot.com"
 
 class Index(webapp.RequestHandler):
     def get(self):
+        ##Dictionary for values
+        self.tv = {}
+
+        ##Get information from spreadsheet
+        self.client = ss.DatabaseClient(username=username,
+                                        password=password)
+        self.db = self.client.GetDatabases(name=spreadsheet)[0]
+        self.get_settings()
+        self.get_divisions()
+
+        ##Generate response
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
-        self.response.out.write(template.render(path, {}))
+        self.response.out.write(template.render(path, self.tv))
+
+    def get_divisions(self):
+        """Populate divisions
+        """
+        self.tv["DIVISIONS"] = ""
+        divtable = self.db.GetTables(name="Divisions")[0]
+        index = 1
+        record = divtable.GetRecord(row_number=index)
+        while (record != None):
+            divstr = '''<div style="'''
+            for k,v in record.content.items():
+                if ((k != "content") and
+                    (v != None)):
+                    divstr += k+":"+v+";"
+            divstr += '''">%s</div>\n''' % record.content["content"]
+            self.tv["DIVISIONS"] += divstr
+
+            index += 1
+            record = divtable.GetRecord(row_number=index)
+
+        return self.tv
+
+    def get_settings(self):
+        """Populate values from Settings table
+        """
+        settingtable = self.db.GetTables(name="Settings")[0]
+        index = 1
+        record = settingtable.GetRecord(row_number=index)
+        while (record != None):
+            if (record.content["item"] == "Google Analytics"):
+                self.tv["google_analytics"] = features.get_google_analytics(record.content["value"])
+            elif (record.content["item"] == "Stylesheet"):
+                self.tv["css_link"] = features.get_css(record.content["value"])
+            else:
+                self.tv[record.content["item"]] = str(record.content["value"])
+
+            index += 1
+            record = settingtable.GetRecord(row_number=index)
+        
+        return self.tv
+    
 
 application = webapp.WSGIApplication([
     ('/', Index)],
---
+++ yapkke/features.py
@@ -0,0 +1,20 @@
+def get_google_analytics(code):
+    """Get code for Google analytics
+    """
+    return '''
+    <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push([\'_setAccount\', \'%s\']);
+    _gaq.push([\'_trackPageview\']);
+    (function() {
+    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    </script>
+    ''' % code
+
+def get_css(cssfile):
+    return '''
+    <link type="text/css" rel="stylesheet" href="%s" />
+    ''' % cssfile
